8cf23150aed09c0264bac0cda3026b666bde6fda
tejvuligonda <tejvuligonda@gmail.com>
2016-02-04 00:13:56 -0800

    Updated the compare files script (py2.7) to check for files with different number of lines

--- Compare Files/Python-2.7/compare_files_py_27.py
+++ Compare Files/Python-2.7/compare_files_py_27.py
@@ -6,21 +6,22 @@ file2_read = open(f2, 'r')
 file1 = file1_read.readlines()
 file2 = file2_read.readlines()
 
-error_list = []
 
 def compare():
+    if (len(file1) != len(file2)):
+        print('NOT EQUAL: The files do not have the same number of lines.')
+        return
     for i in range(len(file1)):
         line1_f1 = file1[i]
         line1_f2 = file2[i]
         if line1_f1 != line1_f2:
             print('ERROR: NOT EQUAL')
-            string_to_print = line1_f1 + '!=' + line1_f2
+            line1_f1 = '"' + ' '.join(line1_f1.split()) + '"'
+            line1_f2 = '"' + ' '.join(line1_f2.split()) + '"'
+            string_to_print = line1_f1 + ' != ' + line1_f2
             print(string_to_print)
-            error_list.append('Error')
-            break
-    if (len(error_list) == 0):
-        print('ALL EQUAL')
-
+            return
+    print('ALL EQUAL')
 
 if __name__ == '__main__':
     compare()
