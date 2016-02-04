8d535fc17741691fc85c540901734a63abbd00c0
tejvuligonda <tejvuligonda@gmail.com>
2016-02-03 22:22:14 -0800

    Added error checking when opening files for command line version of compare_files

--- Compare Files/compare_files_terminal.py
+++ Compare Files/compare_files_terminal.py
@@ -1,16 +1,22 @@
 import sys
+import os.path
 
-f1 = sys.argv[1]
-f2 = sys.argv[2]
+def open_files():
+    f1 = sys.argv[1]
+    f2 = sys.argv[2]
+    if (not (os.path.isfile(f1))):
+        print('The first path does not lead to a valid file.')
+        return
+    if (not (os.path.isfile(f2))):
+        print('The second path does not lead to a valid file.')
+        return
+    with open(f1) as file1_read:
+        file1 = file1_read.readlines()
+    with open(f2) as file2_read:
+        file2 = file2_read.readlines()
+    compare(file1,file2)
 
-with open(f1) as file1_read:
-    file1 = file1_read.readlines()
-
-with open(f2) as file2_read:
-    file2 = file2_read.readlines()
-
-
-def compare():
+def compare(file1,file2):
     if (len(file1) != len(file2)):
         print('NOT EQUAL: The files do not have the same number of lines.')
         return
@@ -27,5 +33,5 @@ def compare():
     print('ALL EQUAL')
 
 if __name__ == '__main__':
-    compare()
+    open_files()
     
